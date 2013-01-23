995a5aaf450c8f5ca0bf0b77b010de98a9cc8fbc
Isay Sosa <yasnet21@gmail.com>
2013-01-23 08:17:58 -0600

    Programa de ecuación

---
+++ Ecuación/Nodo.vb
@@ -0,0 +1,57 @@
+﻿Public Class Nodo
+    Private vValor
+    Private vAnterior, vSiguiente, vDerecho, vIzquierdo As Nodo
+
+    Sub New()
+
+    End Sub
+
+    Sub New(ByVal valor)
+        vValor = valor
+    End Sub
+
+    Public Property Valor()
+        Get
+            Return vValor
+        End Get
+        Set(ByVal value)
+            vValor = value
+        End Set
+    End Property
+
+    Public Property Anterior()
+        Get
+            Return vAnterior
+        End Get
+        Set(ByVal value)
+            vAnterior = value
+        End Set
+    End Property
+
+    Public Property Siguiente()
+        Get
+            Return vSiguiente
+        End Get
+        Set(ByVal value)
+            vSiguiente = value
+        End Set
+    End Property
+
+    Public Property Izquierdo()
+        Get
+            Return vIzquierdo
+        End Get
+        Set(ByVal value)
+            vIzquierdo = value
+        End Set
+    End Property
+
+    Public Property Derecho()
+        Get
+            Return vDerecho
+        End Get
+        Set(ByVal value)
+            vDerecho = value
+        End Set
+    End Property
+End Class
